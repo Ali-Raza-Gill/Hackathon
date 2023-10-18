642c5659740373b85485a0ef77b7aa319321289a
Ali Raza <aliraza137725@gmail.com>
2023-10-18 11:13:34 +0500

    small changes occures

--- src/Pages/Routes.jsx
+++ src/Pages/Routes.jsx
@@ -6,9 +6,9 @@ import Dashboard from "../Pages/Dashboard/Routes"
 export default function Index() {
     return (
         <Routes>
-             <Route path='/*' element={<Frontend />}/>
+            <Route path='/*' element={<Frontend />} />
             {/* <Route path='/auth/*' element={<Auth />} />  */}
-            <Route path='/dashboard/*' element={<Dashboard/>}/>
+            <Route path='/dashboard/*' element={<Dashboard />} />
         </Routes>
     )
 }
